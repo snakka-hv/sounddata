00db10ecf6eb5a8bc209736e6383696ec988bcdd
Sudhir Nakka <sudhir.nakka@hitachivantara.com>
2021-10-28 22:45:02 +0530

    initial commit

---
+++ parent.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('#received_text').hide();
+    $('#btnSend').click(function () {
+
+
+    });
+
+    $('#btnReceive').click(function () {
+        $('#received_text').show();
+    });
+});
+
